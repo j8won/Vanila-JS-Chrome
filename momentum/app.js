b8be53b82f0dd50204ea00b844d6ff2981b5e014
j8won <joowoni99@gmail.com>
2022-03-13 00:53:10 +0900

    #3 JS on the browser

--- momentum/app.js
+++ momentum/app.js
@@ -1,38 +1,8 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  const clickedClass = "clicked";
+  h1.classList.toggle(clickedClass);
 }
 
-function handleMouseEnter() {
-  title.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handelWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowoffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("all goooood");
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handelWindowCopy);
-window.addEventListener("offline", handleWindowoffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
